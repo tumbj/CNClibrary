dad35745b85eb1cbc6b370c62cce1b06989c23ec
tumbj <thanadol.ko@ku.th>
2020-03-17 16:57:28 +0700

    int project , firebase

---
+++ app/src/main/java/com/example/cnclibrary/ui/home/HomeViewModel.java
@@ -0,0 +1,35 @@
+package com.example.cnclibrary.ui.home;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
